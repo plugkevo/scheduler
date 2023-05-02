4ca5b2c8b3722186a818a5de3493639535af317c
plugkevo <munenekevin366@gmail.com>
2023-05-02 15:12:06 +0300

    opening a new activity by presssing a button

--- app/src/main/java/com/example/schedule/MainActivity.kt
+++ app/src/main/java/com/example/schedule/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.schedule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.content.Intent
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<ImageView>(R.id.imageView2)
+
+        imageView.setOnClickListener {
+            val intent = Intent(this, add_page::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/schedule/add_page.kt
@@ -0,0 +1,11 @@
+package com.example.schedule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class add_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_page)
+    }
+}
