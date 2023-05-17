66a9ef1bbc4726a9eef22e1790a7577effe5a7e9
plugkevo <munenekevin366@gmail.com>
2023-05-17 14:10:41 +0300

    id solved

--- app/src/main/java/com/example/schedule/MainActivity.kt
+++ app/src/main/java/com/example/schedule/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.schedule
 
-
 import android.content.Intent
-import android.os.Build.VERSION_CODES.R
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
-
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -16,25 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageView = findViewById<ImageView>(R.id.imageView3)
+        val imageView = findViewById<ImageView>(R.id.fab)
         imageView.setOnClickListener {
             val intent = Intent(this, add_page::class.java)
             startActivity(intent)
         }
 
 
-         lateinit var recyclerView: RecyclerView
+        lateinit var recyclerView: RecyclerView
 
-                recyclerView = findViewById(R.id.taskRecyclerView)
-                recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView = findViewById(R.id.taskRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
-                val items = listOf(
-                    Item("Item 1", "Description 1"),
-                    Item("Item 2", "Description 2"),
-                    Item("Item 3", "Description 3")
-                )
-                val adapter = ItemAdapter(items)
-                recyclerView.adapter = adapter
+        val items = listOf(
+            Item("Item 1", "Description 1"),
+            Item("Item 2", "Description 2"),
+            Item("Item 3", "Description 3")
+        )
+        val adapter = ItemAdapter(items)
+        recyclerView.adapter = adapter
 
 
     }
